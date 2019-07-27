c56c627e32226af5541cc008523d67b02f084ad3
sonicakes <soniaparker1207@gmail.com>
2019-07-27 20:14:35 +1000

    intro text card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import Items from './Items';
+import './index.css';
+import Container from 'react-bootstrap/Container';
 class App extends Component {
 
  
@@ -7,7 +9,10 @@ class App extends Component {
   render(){
     return (
         <div className="App">
-   <Items />
+            <Container>
+            <Items />
+            </Container>
+ 
   
         </div>
     );
--- src/Items.js
+++ src/Items.js
@@ -1,31 +1,50 @@
 import React, {Component} from 'react';
 import Results from './data.json'
 import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+
 
 class Items extends Component {
-   
- 
-
- 
- 
-    
-
   render(){
-
     return (
         <div className="App">
+<Row className="py-3">
 
-        <Container>
+
+{Results.map((res) => (
+<Col xs="3">
+    <Image fluid src={res.image.medium}>
+
+    </Image>
+</Col>
+       ))}
+
+    {Results.map((num) => (
+     <Col xs="9">
+        <Card>
+  <Card.Header as="h5">{num.name}</Card.Header>
+  <Card.Body>
+    <Card.Title>Rating: {num.rating.average}, Genres: {num.genres}</Card.Title>
+    <Card.Text>
+    {/* getting rid of html from JSON response */}
+{num.summary.replace(/<\/?[^>]+>/gi, '')} 
+    </Card.Text>
+    <Button className="text-center" variant="flat">Official Site</Button>
+  </Card.Body>
+</Card>
+</Col>
+            ))}
+   
+</Row>
+
         <Row>
            
-       
-
-        
+    
      {Results[0]._embedded.episodes.map((item, key) => (
-        <Col xs="4">
+        <Col className="mb-3" xs="4">
             
             
 
@@ -34,7 +53,8 @@ class Items extends Component {
     <Card.Body>
       <Card.Title>{item.name}</Card.Title>
       <Card.Text>
-      {item.summary}
+      {/* getting rid of html from JSON response */}
+      {item.summary.replace(/<\/?[^>]+>/gi, '')}
       </Card.Text>
     </Card.Body>
     <Card.Footer>
@@ -51,7 +71,7 @@ class Items extends Component {
     
 
     </Row>
-    </Container>
+ 
          
     </div>
     
