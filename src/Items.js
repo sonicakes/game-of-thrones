87bc068dc7f1df9e325b2a1cf73d20dfa4c97bfe
sonicakes <soniaparker1207@gmail.com>
2019-07-27 18:26:27 +1000

    position 3 cards in a row for episodes

--- src/Items.js
+++ src/Items.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import Results from './data.json'
-import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Items extends Component {
    
@@ -14,18 +17,44 @@ class Items extends Component {
 
     return (
         <div className="App">
-   
 
-     <ul>
-                
-                    
-                    {Results[0]._embedded.episodes.map((item, key) => (
-        <li key={key}>Hello, {item.name} from {item.id}!</li>
+        <Container>
+        <Row>
+           
+       
+
+        
+     {Results[0]._embedded.episodes.map((item, key) => (
+        <Col xs="4">
+            
+            
+
+        <Card  key={key}>
+    <Card.Img variant="top" src={item.image.medium} />
+    <Card.Body>
+      <Card.Title>{item.name}</Card.Title>
+      <Card.Text>
+      {item.summary}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Date Aired: {item.airdate}</small><br/>
+      <small className="text-muted">Season: {item.season}</small><br/>
+      <small className="text-muted">Episode: {item.number}</small>
+    </Card.Footer>
+  </Card>
+  </Col>
+                   
+
        
     ))}
-            </ul>
-            <Button variant="secondary">Go somewhere</Button>
+    
+
+    </Row>
+    </Container>
+         
     </div>
+    
     )
     
   
